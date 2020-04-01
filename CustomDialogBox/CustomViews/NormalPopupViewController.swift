e92a876b32ee0e21161c8d1a6dd9d3832015678b
Shubham0812 <singh.shubham0812@gmail.com>
2020-04-01 14:48:13 +0530

    Initialized Project

---
+++ CustomDialogBox/CustomViews/NormalPopupViewController.swift
@@ -0,0 +1,25 @@
+//
+//  NormalPopup.swift
+//  CustomDialogBox
+//
+//  Created by Shubham Singh on 01/04/20.
+//  Copyright © 2020 Shubham Singh. All rights reserved.
+//
+
+import UIKit
+
+
+class NormalPopupViewController: UIViewController {
+    
+    static let identifier = "NormalPopupViewController"
+    
+    
+    //MARK:- lifecyle methods for the view controller
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+    
+}
+
+
+
