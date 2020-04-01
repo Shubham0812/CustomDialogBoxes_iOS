39b3743ddd42dc337cde2affd6559413073c197d
Shubham0812 <singh.shubham0812@gmail.com>
2020-04-01 19:26:10 +0530

    Comments Added

--- CustomDialogBox/CustomViews/NormalPopupViewController.swift
+++ CustomDialogBox/CustomViews/NormalPopupViewController.swift
@@ -8,46 +8,51 @@
 
 import UIKit
 
-
 class NormalPopupViewController: UIViewController {
     
     static let identifier = "NormalPopupViewController"
     
+    //MARK:- outlets for the viewController
     @IBOutlet weak var dialogBoxView: UIView!
     @IBOutlet weak var okayButton: UIButton!
     
-
     //MARK:- lifecyle methods for the view controller
     override func viewDidLoad(){
         super.viewDidLoad()
-        view.backgroundColor = .white
+        
+        //adding an overlay to the view to give focus to the dialog box
         view.backgroundColor = UIColor.black.withAlphaComponent(0.50)
 
+        //customizing the dialog box view
         dialogBoxView.layer.cornerRadius = 6.0
         dialogBoxView.layer.borderWidth = 1.2
         dialogBoxView.layer.borderColor = UIColor(named: "dialogBoxGray")?.cgColor
         
+        //customizing the okay button
         okayButton.backgroundColor = UIColor(named: "primaryBackground")?.withAlphaComponent(0.85)
         okayButton.setTitleColor(UIColor.white, for: .normal)
-        okayButton.layer.borderWidth = 1.2
         okayButton.layer.cornerRadius = 4.0
+        okayButton.layer.borderWidth = 1.2
         okayButton.layer.borderColor = UIColor(named: "primaryBackground")?.cgColor
-        
     }
     
-    
-    static func showPopup(presentVC: UIViewController){
-        if let popupViewController = UIStoryboard(name: "CustomView", bundle: nil).instantiateViewController(withIdentifier: "NormalPopupViewController") as? NormalPopupViewController {
-            popupViewController.modalPresentationStyle = .custom
-            popupViewController.modalTransitionStyle = .crossDissolve
-            presentVC.present(popupViewController, animated: true)
-        }
-    }
-
+    //MARK:- outlet functions for the viewController
     @IBAction func okayButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
+    //MARK:- functions for the viewController
+    static func showPopup(parentVC: UIViewController){
+        
+        //creating a reference for the dialogView controller
+        if let popupViewController = UIStoryboard(name: "CustomView", bundle: nil).instantiateViewController(withIdentifier: "NormalPopupViewController") as? NormalPopupViewController {
+            popupViewController.modalPresentationStyle = .custom
+            popupViewController.modalTransitionStyle = .crossDissolve
+            
+            //presenting the pop up viewController from the parent viewController
+            parentVC.present(popupViewController, animated: true)
+        }
+    }
 }
 
 
--- CustomDialogBox/CustomViews/PopUpActionViewController.swift
+++ CustomDialogBox/CustomViews/PopUpActionViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+//Protocol to inform the Parent viewController to take some action based on the dialog box
 protocol PopUpProtocol {
     func handleAction(action: Bool)
 }
@@ -18,23 +19,24 @@ class PopUpActionViewController: UIViewController {
     
     var delegate: PopUpProtocol?
     
-    
+    //MARK:- outlets for the view controller
     @IBOutlet weak var dialogBoxView: UIView!
     @IBOutlet weak var gotoStoreButton: UIButton!
     @IBOutlet weak var laterButton: UIButton!
     
-    
-    
+    //MARK:- lifecyle methods for the view controller
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .white
+        //adding an overlay to the view to give focus to the dialog box
         view.backgroundColor = UIColor.black.withAlphaComponent(0.50)
         
+        //customizing the dialog box view
         dialogBoxView.layer.cornerRadius = 6.0
         dialogBoxView.layer.borderWidth = 1.2
         dialogBoxView.layer.borderColor = UIColor(named: "dialogBoxGray")?.cgColor
         
+        //customizing the go to app store button
         gotoStoreButton.backgroundColor = UIColor(named: "primaryBackground")?.withAlphaComponent(0.85)
         gotoStoreButton.setTitleColor(UIColor.white, for: .normal)
         gotoStoreButton.layer.borderWidth = 1.2
@@ -42,22 +44,28 @@ class PopUpActionViewController: UIViewController {
         gotoStoreButton.layer.borderColor = UIColor(named: "primaryBackground")?.cgColor
     }
     
+    //MARK:- outlet functions for the viewController
     @IBAction func goToStorePressed(_ sender: Any) {
         self.delegate?.handleAction(action: true)
         self.dismiss(animated: true, completion: nil)
     }
     
-    
     @IBAction func laterButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    static func showPopup(presentVC: UIViewController){
+    //MARK:- functions for the viewController
+    static func showPopup(parentVC: UIViewController){
+        //creating a reference for the dialogView controller
         if let popupViewController = UIStoryboard(name: "CustomView", bundle: nil).instantiateViewController(withIdentifier: "PopUpActionViewController") as? PopUpActionViewController {
             popupViewController.modalPresentationStyle = .custom
-            popupViewController.delegate = presentVC as? PopUpProtocol
             popupViewController.modalTransitionStyle = .crossDissolve
-            presentVC.present(popupViewController, animated: true)
+            
+            //setting the delegate of the dialog box to the parent viewController
+            popupViewController.delegate = parentVC as? PopUpProtocol
+
+            //presenting the pop up viewController from the parent viewController
+            parentVC.present(popupViewController, animated: true)
         }
     }
     
