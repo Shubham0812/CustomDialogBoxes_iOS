9cd0541c8f435c0be7611ebd8950c33fb64b1cd7
Shubham0812 <singh.shubham0812@gmail.com>
2020-04-01 19:26:42 +0530

    HomeView Controller Changes

--- CustomDialogBox/AnotherViewController.swift
+++ CustomDialogBox/AnotherViewController.swift
@@ -6,4 +6,13 @@
 //  Copyright © 2020 Shubham Singh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class AnotherViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
--- CustomDialogBox/HomeViewController.swift
+++ CustomDialogBox/HomeViewController.swift
@@ -8,33 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController, PopUpProtocol {
-    
+class HomeViewController: UIViewController, PopUpProtocol {
     
     let url: String = "https://www.apple.com/in/ios/app-store/"
     
+    //MARK:- lifecyle methods for the view controller
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    //MARK:- outlets for the view controller
     @IBAction func customPopupPressed(_ sender: Any) {
-        NormalPopupViewController.showPopup(presentVC: self)
+        NormalPopupViewController.showPopup(parentVC: self)
     }
-    
     
     @IBAction func customPopupActionPressed(_ sender: Any) {
-        PopUpActionViewController.showPopup(presentVC: self)
+        PopUpActionViewController.showPopup(parentVC: self)
     }
     
+    //MARK:- functions for the view controller
     func handleAction(action: Bool) {
-        print("The action received", action)
+        
+        //opening a link to the app store if the user clickes on the go to app store button
         if (action) {
             guard let url = URL(string: url) else {
-              return
+                return
             }
             UIApplication.shared.open(url, options: [:], completionHandler: nil)
         }
     }
-    
 }
 
